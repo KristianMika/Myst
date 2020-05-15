e40d7036ca8cab14920f91acbbb27c93eb67f668
Kristian Mika <xmika2@mail.muni.cz>
2020-05-15 16:12:34 +0200

    add tests for queries submitted by hosts with insufficient privileges

--- MPCTestClient/test/mpcclient/MPCTestSimulatedCardInsufficientPrivileges.java
+++ MPCTestClient/test/mpcclient/MPCTestSimulatedCardInsufficientPrivileges.java
@@ -5,11 +5,38 @@ import org.testng.annotations.Test;
 
 public class MPCTestSimulatedCardInsufficientPrivileges {
     @Test
-    void MPCTestSimulatedCardInsufficientPrivileges() throws Exception {
+    void runMPCTestSimulatedCardInsufficientPrivileges() throws Exception {
+        MPCRunConfig runCfg = MPCRunConfig.getDefaultConfig();
+        runCfg.numPlayers = 5;
+        runCfg.numSingleOpRepeats = 1;
+        // Execute once
+        mpctestclient.MPCTestClient.TestMPCProtocol_InsufficientPrivileges(runCfg, MPCRunConfig.CARD_TYPE.JCARDSIMLOCAL);
+    }
+
+    @Test
+    void runMPCTestSimulatedCardInsufficientPrivileges_1_sim_player() throws Exception {
         MPCRunConfig runCfg = MPCRunConfig.getDefaultConfig();
         runCfg.numPlayers = 1;
         runCfg.numSingleOpRepeats = 1;
         // Execute once
-        mpctestclient.MPCTestClient.TestMPCProtocol_InsufficientPrivileges(runCfg, MPCRunConfig.CARD_TYPE.JCARDSIMLOCAL);
+        mpctestclient.MPCTestClient.TestMPCProtocol_InsufficientPrivileges(runCfg, MPCRunConfig.CARD_TYPE.PHYSICAL);
+    }
+
+    @Test
+    void runMPCTestSimulatedCardInsufficientPrivileges_2_sim_players() throws Exception {
+        MPCRunConfig runCfg = MPCRunConfig.getDefaultConfig();
+        runCfg.numPlayers = 2;
+        runCfg.numSingleOpRepeats = 1;
+        // Execute once
+        mpctestclient.MPCTestClient.TestMPCProtocol_InsufficientPrivileges(runCfg, MPCRunConfig.CARD_TYPE.PHYSICAL);
+    }
+
+    @Test
+    void runMPCTestSimulatedCardInsufficientPrivileges_10_sim_players() throws Exception {
+        MPCRunConfig runCfg = MPCRunConfig.getDefaultConfig();
+        runCfg.numPlayers = 10;
+        runCfg.numSingleOpRepeats = 1;
+        // Execute once
+        mpctestclient.MPCTestClient.TestMPCProtocol_InsufficientPrivileges(runCfg, MPCRunConfig.CARD_TYPE.PHYSICAL);
     }
 }
